c1930804159f596a31500021d8918892039b63ee
mogita <me@mogita.com>
2021-03-17 22:31:27 +0800

    feat: initial refurbishment and decorations

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,19 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className='container'>
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>知一网络</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <Header title='宜昌知一网络科技有限公司' />
+        <p className='description'>
+          联系我们{' '}
+          <code className='email'>
+            <a href='mailto:hello@zhyee.xyz'>hello@zhyee.xyz</a>
+          </code>
         </p>
       </main>
 
