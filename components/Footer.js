02bc91621301c987210729a233095cee8f9fdb8f
mogita <me@mogita.com>
2022-09-08 16:47:53 +0800

    feat: remove chatra

--- components/Footer.js
+++ components/Footer.js
@@ -6,23 +6,6 @@ export default function Footer() {
       <footer className={styles.footer}>
         Made with <img src='/netliheart.svg' alt='Netlify Logo' className={styles.logo} /> for you
       </footer>
-
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-    (function(d, w, c) {
-        w.ChatraID = '4MqPoSYc6Ah9ZGyKt';
-        var s = d.createElement('script');
-        w[c] = w[c] || function() {
-            (w[c].q = w[c].q || []).push(arguments);
-        };
-        s.async = true;
-        s.src = 'https://call.chatra.io/chatra.js';
-        if (d.head) d.head.appendChild(s);
-    })(document, window, 'Chatra');
-          `,
-        }}
-      ></script>
     </>
   )
 }
